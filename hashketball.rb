9c504ccd70d277ce6ce2ddeba94ba405e7bf352e
Gregory Richens <gregoryrichens@gmail.com>
2020-04-06 18:02:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -111,7 +111,17 @@ end
 def big_shoe_rebounds
   rebound_high = 0 
   board_man = "Gets Paid"
-  
+  game_hash.each do |(location_key, attribute_hash)|
+    attribute_hash[:players].each do |player_hash|
+      if num_boards_got(player_hash[:player_name]) > rebound_high
+        rebound_high = num_boards_got(player_hash[:player_name])
+        board_man = player_hash[:player_name]
+      end
+    end
+  end
+  rebound_high
+  board_man
+end
   
   
   
